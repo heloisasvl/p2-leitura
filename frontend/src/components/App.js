fb61d9a1e28bd69c1c7b35f53f1fc1bd858cc365
Heloisa Araújo <heloisasvl@gmail.com>
2018-11-01 00:31:47 -0300

    feat: Adiciona método para carregar os posts

--- frontend/src/components/App.js
+++ frontend/src/components/App.js
@@ -1,9 +1,18 @@
 import React, { Component } from 'react';
 import { connect } from "react-redux";
+import { fetchPosts } from '../actions';
 
 import './App.css';
 
 class App extends Component {
+
+  /**
+    * @description Carrega todos os posts imediatamente após a montagem do App
+    */
+  componentDidMount() {
+    this.props.fetchPosts()
+  }
+
   render() {
     return (
       <div className="App"></div>
@@ -11,4 +20,16 @@ class App extends Component {
   }
 }
 
-export default connect()(App);
+const mapStateToProps = (state) => {
+  return {
+    posts: state.posts.posts
+  }
+}
+
+const mapDispatchToProps = (dispatch) => {
+  return {
+    fetchPosts: () => dispatch(fetchPosts())
+  }
+}
+
+export default connect(mapStateToProps, mapDispatchToProps)(App);
