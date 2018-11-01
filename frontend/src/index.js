aba72b01bff31277324f58b92d42b7981477bcfc
Heloisa Araújo <heloisasvl@gmail.com>
2018-11-01 00:05:54 -0300

    feat: Adiciona Middleware

--- frontend/src/index.js
+++ frontend/src/index.js
@@ -1,15 +1,11 @@
 import React from 'react';
 import { render } from 'react-dom'
 import { Provider } from 'react-redux';
-import { createStore } from 'redux';
-import rootReducer from './reducers'
+import configureStore from './utils/configureStore';
 
 import App from './components/App';
 
-const store = createStore(
-  rootReducer, /* preloadedState, */
-  window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__()
-);
+const store = configureStore()
 
 render(
   <Provider store={store}>
