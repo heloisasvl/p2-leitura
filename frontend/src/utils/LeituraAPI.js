a78f80f9e989819d56fbc5620ee6a4352c66e0a9
Heloisa Araújo <heloisasvl@gmail.com>
2018-10-31 23:54:00 -0300

    feat: Adiciona api endpoints

---
+++ frontend/src/utils/LeituraAPI.js
@@ -0,0 +1,24 @@
+
+const api = "http://localhost:3001"
+
+// Generate a unique token for storing your bookshelf data on the backend server.
+let token = localStorage.token
+if (!token)
+  token = localStorage.token = Math.random().toString(36).substr(-8)
+
+const headers = {
+  'Accept': 'application/json',
+  'Authorization': token
+}
+
+// Get all of the categories available for the app.
+export const getAllCategories = () =>
+  fetch(`${api}/categories`, { headers })
+    .then(res => res.json())
+    .then(data => data.categories)
+
+// Get all of the posts.
+export const getAllPosts = () =>
+  fetch(`${api}/posts`, { headers })
+    .then(res => res.json())
+    .then(data => data.posts)
