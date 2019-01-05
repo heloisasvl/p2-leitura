2065f18188cc8522cb03f0973290f9dafd4fdfc9
Heloisa Araújo <heloisasvl@gmail.com>
2019-01-05 15:57:33 -0200

    Styles: Adiciona estilo no App

--- frontend/src/index.js
+++ frontend/src/index.js
@@ -5,6 +5,7 @@ import { Provider } from 'react-redux';
 import configureStore from './utils/configureStore';
 
 import 'bootstrap/dist/css/bootstrap.min.css';
+import './index.css';
 
 import App from './containers/App';
 
