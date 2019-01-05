4335755e9244f60c929b91f3979c419ec3071505
Heloisa Araújo <heloisasvl@gmail.com>
2019-01-05 15:58:05 -0200

    refactor: Remove código obsoleto

--- frontend/src/containers/Home.js
+++ frontend/src/containers/Home.js
@@ -22,12 +22,6 @@ class Home extends Component {
         <div className="jumbotron jumbotron-fluid py-4">
           <div className="container-fluid">
             <h1 className="display-5">Padrão (Root)</h1>
-            <ul className="list-unstyled">
-              <li>deve listar todas as categorias disponíveis, que devem se conectar a uma view de categoria para esta categoria</li>
-              <li>deve listar todas as postagens ordenadas pelo voteScore (começando pela pontuação mais alta)</li>
-              <li>deve ter um controle para modificar o método de ordenação da lista, incluindo, no mínimo, ordenar por voteScore ou ordenar por data de criação</li>
-              <li>deve ter um controle para adicionar novas postagens</li>
-            </ul>
           </div>
         </div>
         <main className="container-fluid">
